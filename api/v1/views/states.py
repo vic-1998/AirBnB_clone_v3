a56177ad90caf15914b300e6815bc4c493f8ad86
vic-1998 <victor.mplaza98@gmail.com>
2021-05-07 20:40:11 -0500

    Add State.

---
+++ api/v1/views/states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""a """
+
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+from flask import jsonify, request, make_response, abort
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def getStates():
+    states = []
+    for state in storage.all("State").values():
+        states.append(state.to_dict())
+    return jsonify(states)
+
+
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
+def getStatesId(state_id):
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
