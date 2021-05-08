379cd0e13f4457fcfc1b45a4489691eb866e7105
mauriciosierrac <2376@holbertonschool.com>
2021-05-08 18:14:02 -0500

    fixed get and delete cities.py

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,58 +7,73 @@ from werkzeug.exceptions import abort
 from api.v1.views import app_views
 from models import storage
 from models.city import City
+from models.state import State
 
-app = Flask(__name__)
 
-@app_views.route('/api/v1/states/<state_id>/cities', methods=['GET', 'POST'])
-def states(state_id):
-    """ 
-    route cities by states id
-    made get and post petition
+@app_views.route('/cities', methods=['GET'], strict_slashes=False)
+def allCities():
+    ''' search all cities'''
+    cities = []
+    for city in storage.all("City").values():
+        cities.append(city.to_dict())
+    return jsonify(cities)
 
-    Args:
-        state_id ([integer]): [id state in DB]
+
+@app_views.route('/states/<string:state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
+def getCitiesByState(state_id):
+    ''' search cities by id '''
+    all_cities = storage.get("State", state_id)
+    if all_cities is None:
+        abort(404)
+    data = []
+    for city in all_cities.cities:
+        data.append(city.to_dict())
+    return jsonify(data)
+
+
+@app_views.route('/cities/<string:city_id>',
+                 methods=['GET'], strict_slashes=False)
+def getCitiesId(city_id):
     """
-    state_obj = storage.get('State', state_id)
-    if state_obj is None:
-        abort(404, 'Not found')
-    
-    if request.method == 'POST':
-        res_json = request.get_json()
-        if res_json is None:
-            abort(400, 'Not a JSON')
-        if res_json.get('name') is None:
-            abort(400, 'Missing name')
-    city = City(**request.get_json())
-    city.save()
-    return make_response(jsonify(city.to_dict()), 201)
-        
-        
-
-@app_views.route('/api/v1/cities/<city_id>/cities', methods=['GET', 'DELETE', 'PUT'])
-def cities(city_id):
-    """
-    route cities, search cities by id cities
-    made get and post petitions
+    Get cities by city Id
     Args:
         city_id ([integer]): [id city in DB]
     """
-    city_obj = storage.get('City',city_id)
-    if city_obj is None:
-        abort(404, 'Not found')
-        
-    if request.method == 'GET':
-        return jsonify(city_obj.to_json())
-    
-    if request.method == 'DELETE':
-        city_obj.delete()
-        del city_obj
-        return jsonify({}), 200
-    
-    if request.method == 'PUT':
-        res_json = request.get_json()
-        if res_json is None:
-            abort(404, 'Not a JSON')
-        city_obj.bm_update(res_json)
-        return jsonify(city_obj.to_json()), 200
-        
+    cityId = storage.get("City", city_id)
+    if cityId is None:
+        abort(404)
+    return jsonify(cityId.to_dict())
+
+
+@app_views.route('/cities/<string:city_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delcity(city_id):
+    """deletes a city"""
+    empty_dict = {}
+
+    try:
+        json_cities = storage.get("City", city_id)
+        json_cities.delete()
+        storage.save()
+        return jsonify(empty_dict), 200
+    except Exception:
+        abort(404)
+
+
+@app_views.route('/states/<string:state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
+def postCity(state_id):
+    """create a new city"""
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'name' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    data = request.get_json()
+    data['state_id'] = state_id
+    new_City = City(**data)
+    storage.save()
+    return make_response(jsonify(new_City.to_dict()), 201)
