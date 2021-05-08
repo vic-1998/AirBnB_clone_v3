bd84c5360007514284ab07695f1c95e960b864f7
mauriciosierrac <2376@holbertonschool.com>
2021-05-07 20:55:21 -0500

    add the comments in functions and methods

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""" App is a Flask instance
+
+    Returns:
+        if not response
+        [error]: [ Not found]
+"""
+
 
 from flask import Flask, jsonify
 import os
@@ -12,10 +19,23 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close(exception):
+    """[call class storage from models]
+
+    Args:
+        exception ([error]): [call storage class]
+    """
     storage.close()
 
 @app.errorhandler(404)
 def not_found(error):
+    """ if not found return a Json string with error
+
+    Args:
+        error ([error]): [log error]
+
+    Returns:
+        [json]: [error message]
+    """
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
