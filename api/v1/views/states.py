02f2a26813aebaf8e5dad0e2943c89a4687d3a8a
vic-1998 <victor.mplaza98@gmail.com>
2021-05-07 20:58:01 -0500

    Changes the permission.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -22,3 +22,12 @@ def getStatesId(state_id):
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_state(state_id):
+    state = storage.get("State", state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
